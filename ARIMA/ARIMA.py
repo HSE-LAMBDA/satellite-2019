55a911c926bd0abe43691f4da7e23571279ca6c6
treacker <36159472+treacker@users.noreply.github.com>
2020-03-11 18:13:58 +0300

    Add files via upload

---
+++ ARIMA/ARIMA.py
@@ -0,0 +1,54 @@
+from sklearn.linear_model import LinearRegression, RidgeCV
+import numpy as np
+from statsmodels.tsa.arima_model import ARIMA
+
+class myARIMA:
+    """
+    Simple auto-regression model
+    """
+
+
+    def __init__(self, data):
+        """
+        Parameters
+        ----------
+        data: timeseries
+            data to fit
+        
+        """
+        self.X = data
+        self.model = ARIMA(self.X, order=(1,1,0))
+        
+
+    def fit(self, **kwargs):
+        """
+        Model fitting
+        Parameters
+        ----------
+        **kwargs: dict
+            Key-words parameters for fitting.
+        """
+        
+        self.results = self.model.fit(disp=-1, trend='nc')
+        
+        
+        
+    def predict(self, steps=1):
+        """
+        Model prediciton
+        Parameters
+        ----------
+        steps: int
+            Number of prediction steps
+        """
+
+        #X_test = self.train_last
+
+        y_pred = np.empty((steps, ), dtype=np.float64)
+        for step in range(steps):
+            y_pred[step] = self.results.predict(len(self.X) + step, len(self.X) + step)[-1]
+            self.X[:-1] = self.X[1:]
+            self.X[-1] = y_pred[step]
+            self.model = ARIMA(self.X, order=(1,1,0))
+            self.results = self.model.fit(disp=-1, trend='nc')
+        return y_pred
